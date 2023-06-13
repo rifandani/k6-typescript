30a16207e33ba1ff5aea868f23fa8e210bb2e289
nriin-rizeki <ipandani2505@gmail.com>
2023-06-13 21:41:08 +0700

    ci: use pnpm, update actions version

--- src/get-200-status-test.ts
+++ src/get-200-status-test.ts
@@ -1,14 +1,15 @@
-import { sleep, check } from 'k6';
-import { Options } from 'k6/options';
+import { check, sleep } from 'k6';
 import http from 'k6/http';
+import { Options } from 'k6/options';
 
-export let options:Options = {
+export let options: Options = {
   vus: 50,
-  duration: '10s'
+  duration: '10s',
 };
 
 export default () => {
   const res = http.get('https://test-api.k6.io');
+
   check(res, {
     'status is 200': () => res.status === 200,
   });
